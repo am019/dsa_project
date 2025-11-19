8b089263c5f060842e7a99c6bd6e63d511575804
am019 <pes1ug24am019@gmail.com>
2025-11-19 17:35:39 +0530

    Update main.c

--- POS_System_final/main.c
+++ POS_System_final/main.c
@@ -4,6 +4,7 @@
 #include "billing.h"
 #include "report.h"
 #include "utils.h"
+void clearInputBuffer();
 
 
 
@@ -15,7 +16,15 @@ void salesMenu() {
         printf("2. Return Items (Partial)\n");
         printf("0. Back to Main Menu\n");
         printf("Enter choice: ");
-        scanf("%d", &ch);
+
+        if (scanf("%d", &ch) != 1) {
+            printf("❌ Invalid input! Please enter a number.\n");
+            clearInputBuffer();  
+            ch = -1;
+            continue;
+        }
+
+        clearInputBuffer(); // remove leftover \n
 
         switch (ch) {
             case 1:
@@ -32,6 +41,7 @@ void salesMenu() {
         }
     } while (ch != 0);
 }
+
 
 #include <stdio.h>
 
